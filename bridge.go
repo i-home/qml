9a248737deb9b49be590c48c80977cdbe534372a
Igor Kmets <homei@mail.ru>
2018-01-08 12:17:46 +0300

    MacExtras dependency added

--- bridge.go
+++ bridge.go
@@ -3,7 +3,7 @@ package qml
 // #cgo CPPFLAGS: -I./cpp
 // #cgo CXXFLAGS: -std=c++0x -pedantic-errors -Wall -fno-strict-aliasing
 // #cgo LDFLAGS: -lstdc++
-// #cgo pkg-config: Qt5Core Qt5Widgets Qt5Quick
+// #cgo pkg-config: Qt5Core Qt5Widgets Qt5Quick Qt5MacExtras
 //
 // #include <stdlib.h>
 //
